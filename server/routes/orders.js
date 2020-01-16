35196f9437daf3837f4c52b1ba2481a606999da2
Mauro Perez <mmauroperez@gmail.com>
2020-01-16 14:03:54 -0300

    type prob

--- server/routes/orders.js
+++ server/routes/orders.js
@@ -1,15 +1,25 @@
 const express = require('express');
 const bcrypt = require('bcrypt');
 const _ = require('underscore');
-const Orden = require('../models/order');
+const Order = require('../models/order');
 const app = express();
+
+app.delete('/orders/delete/all', function(req, res) {
+  Order.deleteMany({}, function (err, orders) {
+    if (err) return res.status(400).json({ ok: false, err });
+    res.json({
+      deleteAll: true,
+      deletedCount: orders.deletedCount,
+    });
+  });
+});
 
 app.get('/orders', function(req, res) {
   let desde = req.query.desde || 0;
   desde = Number(desde);
   let limite = req.query.limite || 50;
   limite = Number(limite);
-  Orden.find(null)
+  Order.find(null)
     .skip(desde)
     .limit(limite)
     .exec((err, orders) => {
@@ -19,7 +29,7 @@ app.get('/orders', function(req, res) {
           err
         });
       }
-      Orden.count(null, (err, length) => {
+      Order.count(null, (err, length) => {
         res.json({
           ok: true,
           orders,
@@ -31,11 +41,12 @@ app.get('/orders', function(req, res) {
 
 app.post('/order', function(req, res) {
   let body = req.body;
-  let orden = new Orden({
+  let orden = new Order({
       merkat: body.merkat,
       number: body.number,
       price: body.price,
       money: body.money,
+      type: body.type
   });
   orden.save((err, ordenDB) => {
     if (err) {
@@ -50,72 +61,5 @@ app.post('/order', function(req, res) {
     });
   });
 });
-//
-// app.put('/usuario/:id', function(req, res) {
-//
-//     let id = req.params.id;
-//     let body = _.pick(req.body, ['nombre', 'email', 'img', 'role', 'estado']);
-//
-//     Orden.findByIdAndUpdate(id, body, { new: true, runValidators: true }, (err, usuarioDB) => {
-//
-//         if (err) {
-//             return res.status(400).json({
-//                 ok: false,
-//                 err
-//             });
-//         }
-//
-//
-//
-//         res.json({
-//             ok: true,
-//             usuario: usuarioDB
-//         });
-//
-//     })
-//
-// });
-//
-// app.delete('/usuario/:id', function(req, res) {
-//
-//
-//     let id = req.params.id;
-//
-//     // Orden.findByIdAndRemove(id, (err, usuarioBorrado) => {
-//
-//     let cambiaEstado = {
-//         estado: false
-//     };
-//
-//     Orden.findByIdAndUpdate(id, cambiaEstado, { new: true }, (err, usuarioBorrado) => {
-//
-//         if (err) {
-//             return res.status(400).json({
-//                 ok: false,
-//                 err
-//             });
-//         };
-//
-//         if (!usuarioBorrado) {
-//             return res.status(400).json({
-//                 ok: false,
-//                 err: {
-//                     message: 'Orden no encontrado'
-//                 }
-//             });
-//         }
-//
-//         res.json({
-//             ok: true,
-//             usuario: usuarioBorrado
-//         });
-//
-//     });
-
-
-
-// });
-
-
 
 module.exports = app;
